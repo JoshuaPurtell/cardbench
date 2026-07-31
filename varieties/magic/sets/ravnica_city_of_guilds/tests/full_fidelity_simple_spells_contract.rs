a27ded4e1a862b3b507f80fc2c9b32d01d096e31
Josh Purtell <jmvpurtell@gmail.com>
2026-07-30 22:36:33 -0400

    Promote Greater Mossdog fidelity

--- varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
@@ -29,6 +29,7 @@ fn full_fidelity_manifest_records_only_ability_complete_cards() {
             "RAV-GATHER-COURAGE",
             "RAV-SEEDS-OF-STRENGTH",
             "RAV-DARKBLAST",
+            "RAV-GREATER-MOSSDOG",
             "RAV-BOROS-SIGNET",
             "RAV-DIMIR-SIGNET",
             "RAV-GOLGARI-SIGNET",
