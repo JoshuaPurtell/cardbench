6e2b47d476e5d798bd7208597e62d43523db866f
Josh Purtell <jmvpurtell@gmail.com>
2026-07-30 23:05:37 -0400

    fix(rav): synchronize Skyknight fidelity contract

--- varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
@@ -44,6 +44,7 @@ fn full_fidelity_manifest_records_only_ability_complete_cards() {
             "RAV-SNAPPING-DRAKE",
             "RAV-GOLIATH-SPIDER",
             "RAV-COURIER-HAWK",
+            "RAV-SKYKNIGHT-LEGIONNAIRE",
         ]
     );
     let definitions = card_definitions();
