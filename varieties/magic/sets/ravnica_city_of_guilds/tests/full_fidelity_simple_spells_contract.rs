d94254dffb401e698b7f9858c8aca0801b5fbd30
Josh Purtell <jmvpurtell@gmail.com>
2026-07-31 13:26:20 -0400

    feat(rav): implement Excruciator damage exception

--- varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
@@ -64,6 +64,7 @@ fn full_fidelity_manifest_records_only_ability_complete_cards() {
             "RAV-FLAME-KIN-ZEALOT",
             "RAV-ORDRUUN-COMMANDO",
             "RAV-INDENTURED-OAF",
+            "RAV-EXCRUCIATOR",
         ]
     );
     let definitions = card_definitions();
