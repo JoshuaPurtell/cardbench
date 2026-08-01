bbd9992b5c14b99b3bc686013212ad18e603edd5
Josh Purtell <jmvpurtell@gmail.com>
2026-08-01 19:39:15 -0400

    green: update doubling coverage contracts

--- varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
@@ -20,6 +20,7 @@ fn full_fidelity_manifest_records_only_ability_complete_cards() {
             "RAV-DROOLING-GROODION",
             "RAV-GOLGARI-ROTWURM",
             "RAV-SCATTER-THE-SEEDS",
+            "RAV-DOUBLING-SEASON",
             "RAV-SCION-OF-THE-WILD",
             "RAV-GUARDIAN-OF-VITU-GHAZI",
             "RAV-LAST-GASP",
