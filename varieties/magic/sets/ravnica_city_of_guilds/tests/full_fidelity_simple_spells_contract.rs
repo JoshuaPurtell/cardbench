2ad5aec2723c790d6e979bb4884ac0a5def0c78e
Josh Purtell <jmvpurtell@gmail.com>
2026-07-31 23:45:46 -0400

    test(rav): reconcile Sadistic Augermage manifest contract

--- varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/full_fidelity_simple_spells_contract.rs
@@ -96,6 +96,7 @@ fn full_fidelity_manifest_records_only_ability_complete_cards() {
             "RAV-VOTARY-OF-THE-CONCLAVE",
             "RAV-GRAVE-SHELL-SCARAB",
             "RAV-UNDERCITY-SHADE",
+            "RAV-SADISTIC-AUGERMAGE",
         ]
     );
     let definitions = card_definitions();
