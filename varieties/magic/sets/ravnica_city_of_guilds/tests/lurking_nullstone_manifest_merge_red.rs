a3c98fbbb08a81c94d4fc8075c2aa87b118d53fc
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 09:00:27 -0400

    test(rav): expose Lurking manifest drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/lurking_nullstone_manifest_merge_red.rs
@@ -0,0 +1,14 @@
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn lurking_nullstone_manifest_has_all_positive_promotions() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 197);
+    for id in [
+        "RAV-LURKING-INFORMANT",
+        "RAV-NULLSTONE-GARGOYLE",
+        "RAV-GOLGARI-GERMINATION",
+        "RAV-CLOUDSTONE-CURIO",
+    ] {
+        assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&id), "missing {id}");
+    }
+}
