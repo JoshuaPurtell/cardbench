ff3b8298c5de3724d48ffbaea9523b9061a34106
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 08:12:27 -0400

    test(rav): expose shock land manifest drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/shock_lands_manifest_merge_red.rs
@@ -0,0 +1,13 @@
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn shock_lands_share_the_combined_full_manifest() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 191);
+    for id in [
+        "RAV-SACRED-FOUNDRY",
+        "RAV-TEMPLE-GARDEN",
+        "RAV-WATERY-GRAVE",
+    ] {
+        assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&id));
+    }
+}
