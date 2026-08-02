c7aac240c0f58c59ef917e842f89d5c877a70400
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 11:32:10 -0400

    test(rav): expose post-promotion coverage partition drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/dizzy_brownscale_coverage_partition_red.rs
@@ -0,0 +1,19 @@
+use std::collections::BTreeSet;
+
+use cardbench_magic_rav::{CardSemanticStatus, RAV_FULL_FIDELITY_DEFINITION_IDS, rav_main_set_catalog};
+
+#[test]
+fn post_promotion_partition_matches_the_catalog() {
+    let full_ids = RAV_FULL_FIDELITY_DEFINITION_IDS
+        .into_iter()
+        .collect::<BTreeSet<_>>();
+    let partial_names = rav_main_set_catalog()
+        .into_iter()
+        .filter_map(|card| match card.semantic_status {
+            CardSemanticStatus::ExecutableCompatibilitySlice { definition_id }
+                if !full_ids.contains(definition_id) => Some(card.name),
+            _ => None,
+        })
+        .collect::<BTreeSet<_>>();
+    assert_eq!(partial_names.len(), 48);
+}
