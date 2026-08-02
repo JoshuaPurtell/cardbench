cd955cfc86c2b339b49ebd473709ee7f41164852
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 09:35:35 -0400

    test(rav): expose Sunforger manifest drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/sunforger_bottled_manifest_merge_red.rs
@@ -0,0 +1,15 @@
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn sunforger_bottled_manifest_has_all_positive_promotions() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 200);
+    for id in [
+        "RAV-SUNFORGER",
+        "RAV-BOTTLED-CLOISTER",
+        "RAV-PARIAHS-SHIELD",
+        "RAV-LURKING-INFORMANT",
+        "RAV-NULLSTONE-GARGOYLE",
+    ] {
+        assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&id), "missing {id}");
+    }
+}
