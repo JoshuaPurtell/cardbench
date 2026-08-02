f905dcec78b680bdc3f68be9527ba8bc4e2c63bb
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 08:57:10 -0400

    test(rav): expose Nullstone manifest drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/nullstone_germination_cloudstone_manifest_merge_red.rs
@@ -0,0 +1,13 @@
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn nullstone_germination_cloudstone_manifest_has_all_promotions() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 196);
+    for id in [
+        "RAV-NULLSTONE-GARGOYLE",
+        "RAV-GOLGARI-GERMINATION",
+        "RAV-CLOUDSTONE-CURIO",
+    ] {
+        assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&id), "missing {id}");
+    }
+}
