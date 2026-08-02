ae229968c23db34964be45fd190349875268b6a0
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 02:00:57 -0400

    test(rav): expose Tattered Drake regeneration gap

--- varieties/magic/sets/ravnica_city_of_guilds/tests/tattered_drake_full_fidelity_red.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/tattered_drake_full_fidelity_red.rs
@@ -1,9 +1,8 @@
-//! Ignored full-fidelity boundary probe for Tattered Drake.
+//! Red full-fidelity boundary probe for Tattered Drake.
 
 use cardbench_magic_rav::{RAV_FULL_FIDELITY_DEFINITION_IDS, card_definitions};
 
 #[test]
-#[ignore = "Tattered Drake's regeneration activation is not implemented"]
 fn tattered_drake_requires_regeneration_for_full_fidelity() {
     let drake = card_definitions()
         .into_iter()
