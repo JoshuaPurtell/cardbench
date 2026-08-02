5847af36f87e427b89dc9196b5fc3b6ff2d0f7c7
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 09:37:37 -0400

    test(rav): expose Plague Sunforger manifest drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/plague_sunforger_manifest_merge_red.rs
@@ -0,0 +1,15 @@
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn plague_sunforger_manifest_has_all_positive_promotions() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 201);
+    for id in [
+        "RAV-PLAGUE-BOILER",
+        "RAV-SUNFORGER",
+        "RAV-BOTTLED-CLOISTER",
+        "RAV-PARIAHS-SHIELD",
+        "RAV-LURKING-INFORMANT",
+    ] {
+        assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&id), "missing {id}");
+    }
+}
