b97b9054f30f11030d65208444977807e1ae70f8
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 15:00:56 -0400

    test(rav): expose Vulturous manifest merge drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/vulturous_manifest_merge_red.rs
@@ -0,0 +1,9 @@
+//! Red merge-contract regression for the Vulturous Zombie promotion.
+
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn vulturous_promotion_reconciles_the_positive_manifest_cardinality() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 228);
+    assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&"RAV-VULTUROUS-ZOMBIE"));
+}
