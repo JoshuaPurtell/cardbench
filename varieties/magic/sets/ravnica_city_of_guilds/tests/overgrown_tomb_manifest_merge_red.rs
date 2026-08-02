56552fead7fc1b4db391ab42f5d11ab3b35fd5b1
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 07:58:06 -0400

    test(rav): reconcile Crown merged manifest contract

--- varieties/magic/sets/ravnica_city_of_guilds/tests/overgrown_tomb_manifest_merge_red.rs
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/overgrown_tomb_manifest_merge_red.rs
@@ -1,8 +1,9 @@
 use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
 
 #[test]
-fn overgrown_tomb_and_reroute_share_the_reconciled_full_manifest() {
-    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 186);
+fn crown_reroute_and_overgrown_tomb_share_the_reconciled_full_manifest() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 187);
+    assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&"RAV-CROWN-OF-CONVERGENCE"));
     assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&"RAV-OVERGROWN-TOMB"));
     assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&"RAV-REROUTE"));
 }
