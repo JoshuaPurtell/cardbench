ba5b6c78330944c913b762f57e2014b9f650e567
Josh Purtell <jmvpurtell@gmail.com>
2026-08-02 08:27:48 -0400

    test(rav): expose Dark Heart manifest drift

---
+++ varieties/magic/sets/ravnica_city_of_guilds/tests/dark_heart_manifest_merge_red.rs
@@ -0,0 +1,7 @@
+use cardbench_magic_rav::RAV_FULL_FIDELITY_DEFINITION_IDS;
+
+#[test]
+fn dark_heart_keeps_the_combined_full_manifest() {
+    assert_eq!(RAV_FULL_FIDELITY_DEFINITION_IDS.len(), 193);
+    assert!(RAV_FULL_FIDELITY_DEFINITION_IDS.contains(&"RAV-DARK-HEART-OF-THE-WOOD"));
+}
